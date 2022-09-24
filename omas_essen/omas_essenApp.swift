10f9893b0a9056898bdd4852174f893632ce9ee4
BrokenBladee <47634664+BrokenBladee@users.noreply.github.com>
2022-09-24 13:50:30 +0200

    Initial Commit

---
+++ omas_essen/omas_essenApp.swift
@@ -0,0 +1,17 @@
+//
+//  omas_essenApp.swift
+//  omas_essen
+//
+//  Created by Laurin Notemann on 24.09.22.
+//
+
+import SwiftUI
+
+@main
+struct omas_essenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

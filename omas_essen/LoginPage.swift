a60ef8708a7921a3408418c94dbb51a7366c985e
BrokenBladee <47634664+BrokenBladee@users.noreply.github.com>
2022-09-24 15:28:43 +0200

    Changes to LoginPage

--- omas_essen/LoginPage.swift
+++ omas_essen/LoginPage.swift
@@ -8,8 +8,27 @@
 import SwiftUI
 
 struct LoginPage: View {
+    @State private var name: String = ""
+    @State private var phonenumber: String = ""
+    @State private var summary: String = ""
     var body: some View {
-        Text("Login:")
+        VStack(alignment: .center){
+            Text("Login:")
+            TextField(
+                    "Enter your full name",
+                    text: $name
+            )
+            .border(.secondary)
+            TextField(
+                "Enter your mobile number",
+                text: $phonenumber
+            )
+            TextField(
+                "Tell something about yourself",
+                text: $summary
+            )
+        }
+        
     }
 }
 

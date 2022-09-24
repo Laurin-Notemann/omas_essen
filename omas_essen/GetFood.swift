95aeee055f7fbdd0d02783a8847f45352a2451d7
BrokenBladee <47634664+BrokenBladee@users.noreply.github.com>
2022-09-24 14:49:17 +0200

    Added File

---
+++ omas_essen/GetFood.swift
@@ -0,0 +1,20 @@
+//
+//  GetFood.swift
+//  omas_essen
+//
+//  Created by Laurin Notemann on 24.09.22.
+//
+
+import SwiftUI
+
+struct GetFood: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GetFood_Previews: PreviewProvider {
+    static var previews: some View {
+        GetFood()
+    }
+}
